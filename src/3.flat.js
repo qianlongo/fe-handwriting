911416d602153f046023721cf805a150b33d5850
qianlongo <1162633511@qq.com>
2021-07-18 10:23:46 +0800

    feat: 数组扁平化

--- src/3.flat.js
+++ src/3.flat.js
@@ -1,3 +1,4 @@
+// 递归实现
 const flat1 = (array) => {
   return array.reduce((result, it) => {
     return result.concat(Array.isArray(it) ? flat1(it) : it)
@@ -10,3 +11,15 @@ let arr1 = [
   [ 5, [ 6, [ 7, [ 8 ] ] ] ]
 ]
 console.log(flat1(arr1))
+
+// js原生的flat方法
+const flat2 = (array) => {
+  return array.flat(Infinity)
+}
+
+let arr2 = [
+  1,
+  [ 2, 3, 4 ],
+  [ 5, [ 6, [ 7, [ 8 ] ] ] ]
+]
+console.log(flat2(arr2))
