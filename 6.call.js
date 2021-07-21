58bd8c4d0104d3488e043799992d08768d57a465
qianlongo <1162633511@qq.com>
2021-07-21 23:35:43 +0800

    feat: 模拟实现call

---
+++ 6.call.js
@@ -0,0 +1,24 @@
+// https://github.com/mqyqingfeng/Blog/issues/11
+Function.prototype.myCall = function (ctx, ...args) {
+  if (!ctx) {
+    ctx = typeof window !== 'undefined' ? window : global
+  }
+  // 暴露处理 ctx有可能传非对象
+  ctx = Object(ctx)
+
+  const fnName = Symbol()
+
+  ctx[ fnName ] = this
+
+  const result = ctx[ fnName ](...args)
+
+  delete ctx[ fnName ]
+
+  return result
+}
+
+let fn = function (name) {
+  console.log(this, name)
+}
+
+fn.myCall('', 'qianlongo')
