75613ed2490381cddd8103a2b0501917a43b9a56
qianlongo <1162633511@qq.com>
2021-10-05 23:03:58 +0800

    feat: 实现出版JSON.stringify

--- 50.JSON.stringify.js
+++ 50.JSON.stringify.js
@@ -1,3 +1,77 @@
+const jsonstringify = (data) => {
+  const type = typeof data
+  const commonKeys1 = [ 'undefined', 'function', 'symbol' ]
+   
+  // 非对象
+  if (type !== 'object' || data === null) {
+    let result = ''
+    // 特性四：
+    // NaN 和 Infinity 格式的数值及 null 都会被当做 null。
+    if ([ NaN, Infinity, null ].includes(data)) {
+      result = 'null'
+      // 特性一：
+      // `undefined`、`任意的函数`以及`symbol值`被`单独转换`时，会返回 undefined
+    } else if (commonKeys1.includes(type)) {
+      // 直接得到undefined，并不是一个字符串'undefined'
+      return undefined
+    } else {
+      result = `${data}`
+    }
+
+    return String(result)
+  } else if (type === 'object') {
+    // 特性五:
+    // 转换值如果有 toJSON() 方法，该方法定义什么值将被序列化
+    if (typeof data.toJSON === 'function') {
+      return jsonstringify(data.toJSON())
+    } else if (Array.isArray(data)) {
+      let result = data.map((it) => {
+        // 特性一:
+        // `undefined`、`任意的函数`以及`symbol值`出现在`数组`中时会被转换成 `null`
+        return commonKeys1.includes(typeof it) ? 'null' : jsonstringify(it)
+      })
+
+      return `[${result}]`.replace(/'/g, '"')
+    } else {
+      // 处理普通对象
+      let result = []
+
+      Object.keys(data).forEach((key) => {
+        // 特性三
+        // 所有以symbol为属性键的属性都会被完全忽略掉，即便 replacer 参数中强制指定包含了它们。
+        if (typeof key !== 'symbol') {
+          const value = data[ key ]
+          // 特性一
+          // `undefined`、`任意的函数`以及`symbol值`，出现在`非数组对象`的属性值中时在序列化过程中会被忽略
+          if (!commonKeys1.includes(typeof value)) {
+            result.push(`${key}:${jsonstringify(value)}`)
+          }
+        }
+      })
+
+      return `{${result}}`.replace(/'/, '"')
+    }
+  }
+  
+}
+
+console.log(jsonstringify(undefined))
+console.log(jsonstringify(() => {}))
+console.log(jsonstringify(Symbol('前端胖头鱼')))
+console.log(jsonstringify((NaN)))
+console.log(jsonstringify((Infinity)))
+console.log(jsonstringify((null)))
+console.log(jsonstringify({
+  name: '前端胖头鱼',
+  toJSON () {
+    return {
+      name: '前端胖头鱼2',
+      sex: 'boy'
+    }
+  }
+}))
+
+/*
 // 1. 转换对象
 console.log(JSON.stringify({ name: '前端胖头鱼', sex: 'boy' }))
 // 2. 转换普通值
@@ -112,3 +186,4 @@ console.log(JSON.stringify(enumerableObj))
 const alsoHuge = BigInt(9007199254740991)
 
 console.log(JSON.stringify(alsoHuge))
+*/
