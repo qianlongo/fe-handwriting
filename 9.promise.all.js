64bbccd910db2e87bebae8d84f8e06479db0ded9
qianlongo <1162633511@qq.com>
2021-12-05 22:20:39 +0800

    feat: Promise.all

--- 9.promise.all.js
+++ 9.promise.all.js
@@ -17,6 +17,46 @@ Promise.myAll = (promises) => {
   })
 }
 
+Promise.myAll2 = (promises) => {
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(promises)) {
+      return reject('Promise.all accepts an array')
+    }
+
+    const length = promises.length
+    let remaining = length
+    const result = []
+
+    if (length === 0) {
+      return resolve([])
+    }
+
+    const response = (i, value) => {
+      try {
+        if (value instanceof Promise) {
+          // 注意此val非value
+          value.then((val) => {
+            response(i, val)
+          }, reject)
+          return
+        }
+
+        result[ i ] = value
+
+        if (--remaining === 0) {
+          resolve(result)
+        }
+      } catch (err) {
+        reject(err)
+      }
+    }
+
+    for (let i = 0; i < length; i++) {
+      response(i, promises[i])
+    }
+  })
+}
+
 let p1 = Promise.resolve(1);
 let p2 = 2;
 let p3 = new Promise((resolve, reject) => {
@@ -25,19 +65,37 @@ let p3 = new Promise((resolve, reject) => {
 let p4 = Promise.reject('出错啦')
 
 
-Promise.myAll([ p1, p2, p3 ]).then((res) => {
-  console.log(res, 'res---1')
-}).catch((err) => {
-  console.log('err', err)
-})
+// Promise.myAll([ p1, p2, p3 ]).then((res) => {
+//   console.log(res, 'res---1')
+// }).catch((err) => {
+//   console.log('err', err)
+// })
 
-Promise.myAll([ p1, p2, 3 ]).then((res) => {
-  console.log(res, 'res---2')
-}).catch((err) => {
-  console.log('err', err)
-})
+// Promise.myAll([ p1, p2, 3 ]).then((res) => {
+//   console.log(res, 'res---2')
+// }).catch((err) => {
+//   console.log('err', err)
+// })
 
-Promise.myAll([ p1, p2, p4 ]).then((res) => {
+// Promise.myAll([ p1, p2, p4 ]).then((res) => {
+//   console.log(res, 'res--3')
+// }).catch((err) => {
+//   console.log('err', err)
+// })
+
+// Promise.myAll2([ p1, p2, p3 ]).then((res) => {
+//   console.log(res, 'res---1')
+// }).catch((err) => {
+//   console.log('err', err)
+// })
+
+// Promise.myAll2([ p1, p2, 3 ]).then((res) => {
+//   console.log(res, 'res---2')
+// }).catch((err) => {
+//   console.log('err', err)
+// })
+
+Promise.myAll2([ p1, p2, p4 ]).then((res) => {
   console.log(res, 'res--3')
 }).catch((err) => {
   console.log('err', err)
