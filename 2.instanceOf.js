6e6e3cfa45411f048162f21e007bae3c88a22a1b
qianlongo <1162633511@qq.com>
2021-12-26 15:32:16 +0800

    feat: instanceOf修复对函数的判断

--- 2.instanceOf.js
+++ 2.instanceOf.js
@@ -5,10 +5,10 @@
  * @returns true false
  */
 const instanceOf1 = (obj, func) => {
-  if (obj === null || typeof obj !== 'object') {
+  if (!(obj && ['object', 'function'].includes(typeof obj))) {
     return false
   }
-
+  
   let proto = Object.getPrototypeOf(obj)
 
   if (proto === func.prototype) {
@@ -26,7 +26,7 @@ const instanceOf1 = (obj, func) => {
  * @returns true false
  */
 const instanceOf2 = (obj, func) => {
-  if (obj === null || typeof obj !== 'object') {
+  if (!(obj && ['object', 'function'].includes(typeof obj))) {
     return false
   }
 
@@ -62,6 +62,7 @@ console.log(instanceOf1(p1, Fn))
 console.log(instanceOf1({}, Fn))
 console.log(instanceOf1(null, Fn))
 console.log(instanceOf1(1, Fn))
+console.log(instanceOf1(function a() {}, Function))
 
 console.log(11111111)
 
@@ -71,3 +72,4 @@ console.log(instanceOf2(p1, Fn))
 console.log(instanceOf2({}, Fn))
 console.log(instanceOf2(null, Fn))
 console.log(instanceOf2(1, Fn))
+console.log(instanceOf1(function a() {}, Function))
