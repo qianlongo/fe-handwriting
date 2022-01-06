4b77512ea971a2748320f39c335aa99a5ae7a093
qianlongo <1162633511@qq.com>
2022-01-06 22:33:58 +0800

    feat: 修复trim函数BUG

--- 12.trim.js
+++ 12.trim.js
@@ -1,12 +1,14 @@
 String.prototype.strim1 = function () {
-  return this.replace(/^\s+|\s+$/, '')
+  return this.replace(/^\s+|\s+$/g, '')
 }
 String.prototype.strim2 = function () {
   return this.replace(/^\s+(.*?)\s+$/, '$1')
 }
 
-let str = '     dsfd    '
+let str = '    aaaa   '
+
+console.log(str.length)
 
 console.log(str)
-console.log(str.strim1())
-console.log(str.strim2())
+console.log(str.strim1().length)
+console.log(str.strim2().length)
