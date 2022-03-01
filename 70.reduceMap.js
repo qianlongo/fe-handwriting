3a7d7fe236890990e10bef10f1e763ba9e6c86dd
qianlongo <1162633511@qq.com>
2022-03-01 10:39:04 +0800

    feat: reduceMap

---
+++ 70.reduceMap.js
@@ -0,0 +1,17 @@
+Array.prototype.map2 = function (callback, ctx = null) {
+  if (typeof callback !== 'function') {
+    throw('callback must be a function')
+  }
+
+  return this.reduce((result, cur, index, array) => {
+    return  result.concat(callback.call(ctx, cur, index, array))
+  }, [])
+}
+
+let arr = [ 1, 2 ]
+let arr2 = arr.map2(function (it, i, array) {
+  console.log(it, i, array, this)
+  return it * 2
+}, { name: '前端胖头鱼' })
+
+console.log(arr2) 
